56c0c39a75dfcc06635374bd47953720b3f7f151
Benji936 <50105585+Benji936@users.noreply.github.com>
2025-02-03 00:49:57 +0100

    Initialization
    Starting creating a spring Boot project.

---
+++ main/src/test/java/demo/rad/ar/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.rad.ar.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
