b925afb387ae49a45f8d99fed4cb0dd1679f8977
Benji936 <50105585+Benji936@users.noreply.github.com>
2025-02-09 15:31:35 +0100

    User on Map
    Created a map where we can see user location.

--- radar-dashboard/src/App.vue
+++ radar-dashboard/src/App.vue
@@ -3,7 +3,8 @@
     <h1 class="text-3xl font-bold mb-6 text-center">User Sessions Dashboard</h1>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
       <SessionTable />
-      <SessionChart />
+      <UserMap/>
+      <!--<SessionChart />-->
     </div>
   </div>
 </template>
@@ -11,9 +12,10 @@
 <script>
 import SessionTable from "./components/SessionTable.vue";
 import SessionChart from "./components/SessionChart.vue";
+import UserMap from "./components/UserMap.vue";
 
 export default {
-  components: { SessionTable, SessionChart },
+  components: { SessionTable, SessionChart, UserMap },
 };
 </script>
 
